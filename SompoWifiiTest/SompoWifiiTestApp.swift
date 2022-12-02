863389f0a19d2f9d86f345069d6f75520d624171
ML - Harol <harol.higuera@monstar-lab.com>
2022-12-02 14:49:44 +0900

    Initial Commit

---
+++ SompoWifiiTest/SompoWifiiTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SompoWifiiTestApp.swift
+//  SompoWifiiTest
+//
+//  Created by Harol Higuera on 2022/12/02.
+//
+
+import SwiftUI
+
+@main
+struct SompoWifiiTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
