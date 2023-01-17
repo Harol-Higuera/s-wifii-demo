3815a782d64397f07bac9d4bd04e26eacc6c6095
ML - Harol <harol.higuera@monstar-lab.com>
2023-01-17 12:27:11 +0900

    Added manual

--- SompoWifiiTest/UI/Screens/Main/MainScreen.swift
+++ SompoWifiiTest/UI/Screens/Main/MainScreen.swift
@@ -6,77 +6,93 @@
 //
 
 import SwiftUI
+import PDFKit
 
 struct MainScreen: View {
     @ObservedObject private(set) var viewModel: ViewModel
     
+    @State private var showingChildView = false
+    
     var body: some View {
         ZStack {
             NavigationView {
-                if (viewModel.isConnectedToWifii == nil) {
-                    Text("Loading...")
-                } else {
-                    let isConnectedToWifii = viewModel.isConnectedToWifii ?? true
-                    
-                    if (!isConnectedToWifii) {
+                ZStack {
+                    NavigationLink(
+                        "",
+                        destination: PDFViewer(url: Bundle.main.url(forResource: "manual", withExtension: "pdf")!),
+                        isActive: self.$showingChildView)
+                    if (viewModel.isConnectedToWifii == nil) {
+                        Text("Loading...")
+                    } else {
+                        let isConnectedToWifii = viewModel.isConnectedToWifii ?? true
                         
-                        VStack {
+                        if (!isConnectedToWifii) {
+                            
+                            VStack {
+                                Form {
+                                    Section(header: Text("SSID")) {
+                                        TextField("SSID", text:  $viewModel.entrySsid)
+                                            .padding(.all)
+                                            .background(Color(red: 245, green: 245, blue: 245))
+                                    }
+                                    Section(header: Text("パスワード")) {
+                                        TextField("パスワード", text:  $viewModel.entryPassword)
+                                            .padding(.all)
+                                            .background(Color(red: 245, green: 245, blue: 245))
+                                    }
+                                    
+                                    Section(header: Text("履歴")) {
+                                        DevicesList(models: viewModel.devices, viewModel: viewModel, onPopulateFields: { model in
+                                            viewModel.populateFields(model: model)
+                                        })
+                                    }
+                                }
+                                .navigationBarItems(
+                                    leading: Button(action:{
+                                        self.showingChildView = true
+                                    }) {
+                                        Text("ヘルプ")
+                                            .foregroundColor(Color.accentColor)
+                                    },
+                                    
+                                    trailing: Button(action: {
+                                        viewModel.registerDevice(
+                                            ssid: viewModel.entrySsid,
+                                            password: viewModel.entryPassword) { errorMessage in
+                                                let model = AppAlertModel(toNotice: errorMessage, onOkClicked: {
+                                                    viewModel.resetAlertModel()
+                                                })
+                                                viewModel.updateAlertModel(appAlertModel: model)
+                                            }
+                                    }) {
+                                        Text("登録")
+                                            .foregroundColor(Color.accentColor)
+                                    }
+                                )
+                            }
+                            .navigationTitle("ホーム")
+                            .navigationBarTitleDisplayMode(.inline)
+                            
+                            
+                        } else {
                             Form {
                                 Section(header: Text("SSID")) {
-                                    TextField("SSID", text:  $viewModel.entrySsid)
+                                    Text(viewModel.deviceSSid)
                                         .padding(.all)
                                         .background(Color(red: 245, green: 245, blue: 245))
-                                }
-                                Section(header: Text("パスワード")) {
-                                    TextField("パスワード", text:  $viewModel.entryPassword)
-                                        .padding(.all)
-                                        .background(Color(red: 245, green: 245, blue: 245))
-                                }
-                                
-                                Section(header: Text("履歴")) {
-                                    DevicesList(models: viewModel.devices, viewModel: viewModel, onPopulateFields: { model in
-                                        viewModel.populateFields(model: model)
-                                    })
                                 }
                             }
+                            .navigationTitle("ホーム")
+                            .navigationBarTitleDisplayMode(.inline)
                             .navigationBarItems(
                                 trailing: Button(action: {
-                                    viewModel.registerDevice(
-                                        ssid: viewModel.entrySsid,
-                                        password: viewModel.entryPassword) { errorMessage in
-                                            let model = AppAlertModel(toNotice: errorMessage, onOkClicked: {
-                                                viewModel.resetAlertModel()
-                                            })
-                                            viewModel.updateAlertModel(appAlertModel: model)
-                                        }
+                                    viewModel.disconnectDevice()
                                 }) {
-                                    Text("登録")
+                                    Text("切る")
                                         .foregroundColor(Color.accentColor)
                                 }
                             )
                         }
-                        .navigationTitle("未登録")
-                        .navigationBarTitleDisplayMode(.inline)
-                        
-                        
-                    } else {
-                        Form {
-                            Section(header: Text("SSID")) {
-                                Text(viewModel.deviceSSid)
-                                    .padding(.all)
-                                    .background(Color(red: 245, green: 245, blue: 245))
-                            }
-                        }
-                        .navigationTitle("登録済み")
-                        .navigationBarTitleDisplayMode(.inline)
-                        .navigationBarItems(
-                            trailing: Button(action: {
-                                viewModel.disconnectDevice()
-                            }) {
-                                Text("切る")
-                                    .foregroundColor(Color.accentColor)
-                            }
-                        )
                     }
                 }
             }
@@ -155,5 +171,31 @@ struct DevicetItem: View {
         .foregroundColor(.gray)
         .contentShape(Rectangle())
         .padding(.vertical, 20)
+    }
+}
+
+struct PDFViewer: View {
+    var url: URL
+    
+    var body: some View {
+        PDFKitRepresentedView(url)
+    }
+}
+
+struct PDFKitRepresentedView: UIViewRepresentable {
+    let url: URL
+    init(_ url: URL) {
+        self.url = url
+    }
+
+    func makeUIView(context: UIViewRepresentableContext<PDFKitRepresentedView>) -> PDFKitRepresentedView.UIViewType {
+        let pdfView = PDFView()
+        pdfView.document = PDFDocument(url: self.url)
+        pdfView.autoScales = true
+        return pdfView
+    }
+
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<PDFKitRepresentedView>) {
+        
     }
 }
